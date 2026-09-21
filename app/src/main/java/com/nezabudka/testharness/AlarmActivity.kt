a4d6aa55b2872e776c91e053daeb461636b285f4
obermot <325178940+obermot@users.noreply.github.com>
2026-09-21 22:37:56 +0300

    Fix alarm TextButton import

--- app/src/main/java/com/nezabudka/testharness/AlarmActivity.kt
+++ app/src/main/java/com/nezabudka/testharness/AlarmActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
